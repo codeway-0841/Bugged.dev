fb61d49d6e8ac6ae512fa4c76516f9a6c71a06ad
M. Bagher Abiat <zorofight94@gmail.com>
2020-09-06 23:45:58 +0430

    SEO

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ posts }) {
 				<article key={slug}>
 					<header className='mb-2'>
 						<h3 className='mb-2'>
-							<Link href={'/post/[slug]'} as={`/post/${slug}`}>
+							<Link href={'/post/[slug]'} prefetch={true} as={`/post/${slug}`}>
 								<a className='text-3xl font-bold text-red-600 font-display'>{title}</a>
 							</Link>
 						</h3>
