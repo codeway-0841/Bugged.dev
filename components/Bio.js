f5b8fa90d4db306d790e1571c89fdaecef1971a7
M. Bagher Abiat <zorofight94@gmail.com>
2020-09-06 17:40:35 +0430

    New theme

--- components/Bio.js
+++ components/Bio.js
@@ -1,26 +1,25 @@
-import clsx from "clsx";
+import clsx from 'clsx';
 
-import Image from "./Image";
-import { getSiteMetaData } from "utils/helpers";
-
+import Image from './Image';
+import { getSiteMetaData } from 'utils/helpers';
+import Profile from '../content/assets/profile.jpg';
+import PreviewProfile from '../content/assets/profile.jpg?lqip';
 export default function Bio({ className }) {
-  const { author, social } = getSiteMetaData();
-
-  return (
-    <div className={clsx(`flex items-center`, className)}>
-      <Image
-        className="flex-shrink-0 mb-0 mr-3 rounded-full w-14 h-14"
-        src={require("../content/assets/profile.png")}
-        previewSrc={require("../content/assets/profile.png?lqip")}
-        alt="Profile"
-      />
-      <p className="text-base leading-7">
-        Written by <b className="font-semibold">{author.name}</b>{" "}
-        {author.summary}{" "}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          Follow him on twitter
-        </a>
-      </p>
-    </div>
-  );
+	const { author, social } = getSiteMetaData();
+	return (
+		<div className={`flex items-center ${className}`}>
+			<Image
+				className='flex-shrink-0 mb-0 mr-3 rounded-full w-16 h-16'
+				src={Profile}
+				previewSrc={PreviewProfile}
+				alt='Profile'
+			/>
+			<p className='text-base leading-7'>
+				Written by <b className='font-semibold'>{author.name}</b> {author.summary}
+				<br />Follow him on
+				<a href={`https://twitter.com/${social.twitter}`}> Twitter</a> and
+				<a href={`https://github.com/${social.github}`}> Github</a>.
+			</p>
+		</div>
+	);
 }
