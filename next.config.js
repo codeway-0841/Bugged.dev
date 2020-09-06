f402552293840edf35e3d2dc8920f7daeebf0f1c
M. Bagher Abiat <zorofight94@gmail.com>
2020-09-06 21:40:21 +0430

    CDN

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const isProd = process.env.NODE_ENV === 'production';
-module.exports = withPlugins(
-	[
-		optimizedImages
-	],
-	{ assetPrefix: isProd ? 'https://cdn.bugged.dev' : '' }
-);
+module.exports = withPlugins([
+	optimizedImages
+]);
