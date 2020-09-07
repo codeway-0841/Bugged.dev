96ceb9de306436f2b6e73fd45998305c4be1c1cd
M. Bagher Abiat <zorofight94@gmail.com>
2020-09-07 09:49:45 +0430

    Bundle analyzing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled : process.env.ANALYZE === 'true'
+});
+
 module.exports = withPlugins([
+	withBundleAnalyzer({}),
 	optimizedImages
 ]);
