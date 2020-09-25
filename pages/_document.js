23170d634211c14966e90e66b38884433e302402
M. Bagher Abiat <zorofight94@gmail.com>
2020-09-25 16:29:34 +0330

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,14 @@ export default class MyDocument extends Document {
 
     return (
       <Html lang={siteMetadata.language}>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://aslemammad-ackee.herokuapp.com/tracker.js"
+            data-ackee-server="https://aslemammad-ackee.herokuapp.com"
+            data-ackee-domain-id="7134e287-4738-4ca8-89c7-069ef8abf7f4"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
