6cddd840b359464dd8224df2aaf917112a03e4b5
M. Bagher Abiat <zorofight94@gmail.com>
2020-10-20 11:19:26 +0330

    new analytics

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ export default class MyDocument extends Document {
 						data-ackee-server="https://aslemammad-ackee.herokuapp.com"
 						data-ackee-domain-id="7134e287-4738-4ca8-89c7-069ef8abf7f4"
 					/>
+					<script async src="https://cdn.splitbee.io/sb.js" />
 					<script dangerouslySetInnerHTML={{ __html: `window.__onDarkModeChange = function () {};` }} />
 				</Head>
 
