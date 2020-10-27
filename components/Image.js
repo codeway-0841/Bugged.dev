62db6edc2a87040e9d326b4caf508e9fa36fe0ab
M. Bagher Abiat <zorofight94@gmail.com>
2020-10-27 23:47:51 +0330

    Next new version

--- components/Image.js
+++ components/Image.js
@@ -2,9 +2,9 @@ import 'lazysizes';
 
 export default function Image({ alt, src, previewSrc, className, post }) {
 	return (
-		<span className='block text-center text-red text-1xl'>
+		<span className="block text-center text-red text-1xl">
 			<img className={`lazyload blur-up ${className}`} alt={alt} src={previewSrc} data-srcset={src} />
-			<span className='mt-2'>{post ? alt : ''}</span>
+			<span className="mt-2">{post ? alt : ''}</span>
 		</span>
 	);
 }
